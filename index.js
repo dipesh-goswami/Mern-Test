6df7ae8fb9d93d6880b97ee4b10b86575df11d26
dipesh goswami <dipeshgoswami1999@gmail.com>
2024-01-09 14:56:47 +0530

    testing MERN

---
+++ index.js
@@ -0,0 +1,25 @@
+//importing as function
+// import { diff } from './lib.js';
+// console.log(diff(3, 5));
+
+
+//importing as variable
+// const libe = require('./lib.js');
+// console.log(libe.sum(1, 2));
+
+// //using fs
+// const fs = require('fs');
+// // const txt = fs.readFileSync('demo.txt', 'utf-8');
+// // console.log(txt);
+
+// fs.readFile('demo.txt', 'utf-8', (err, txt) => {
+//   console.log(txt);
+// })
+
+
+
+console.log("hello dipesh");
+const express = require('express');
+const server = express();
+server.listen(8080);
+
---
+++ lib.js
@@ -0,0 +1,11 @@
+// //exporting directing by making variable function
+// exports.sum = (a, b) => {
+//   return a + b;
+// }
+
+
+// //exporting by first making a variable and then using export funtion to export it.
+// const diff = (a, b) => {
+//   return a - b;
+// }
+// export { diff };
