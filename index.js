b6f2e2700be7c10511f01c12211a3eb39cbb093f
dipesh goswami <dipeshgoswami1999@gmail.com>
2024-01-09 17:46:01 +0530

    Node modules that has been tested are: Express, Os, File System. Callback, Promises and async-await functionalities are tested.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-//importing as function
+//**********importing as function***************
 // import { diff } from './lib.js';
 // console.log(diff(3, 5));
 
 
-//importing as variable
+//************importing as variable******************
 // const libe = require('./lib.js');
 // console.log(libe.sum(1, 2));
 
-// //using fs
+// //***************using fs**********************
 // const fs = require('fs');
 // // const txt = fs.readFileSync('demo.txt', 'utf-8');
 // // console.log(txt);
@@ -17,18 +17,55 @@
 // })
 
 
-
+//**********using express**************
 // console.log("hello dipesh");
 // const express = require('express');
 // const server = express();
 // server.listen(8080);
 
-const os = require('os');
-// console.log(os.EOL);//cursor go to next line
-// console.log(os.arch());//tells your os arch...x64
-// console.log(os.cpus());
-// console.log(os.devNull);
-// console.log(os.freemem());
-// console.log(os.homedir());
-// console.log(os.networkInterfaces());
-console.log(os.version());
+
+//**************using os*****************
+// const os = require('os');
+// // console.log(os.EOL);//cursor go to next line
+// // console.log(os.arch());//tells your os arch...x64
+// // console.log(os.cpus());
+// // console.log(os.devNull);
+// // console.log(os.freemem());
+// // console.log(os.homedir());
+// // console.log(os.networkInterfaces());
+// console.log(os.version());
+
+
+//**********promises******************
+// const p = new Promise((res, rej) => {
+//   rej("1211212");
+// });
+
+// p.then((data) => {
+//   console.log("resolved 1");
+//   return data;
+// }).then(() => {
+//   console.log("resolved 2")
+// }).catch((err) => {
+//   console.log("rejected 1");
+//   return err;
+// }).then((err) => {
+//   console.log("rejected 2");
+// });
+
+
+
+//*****************async await****************
+// const p = new Promise((res, rej) => {
+//   setTimeout(() => { res(6) }, 2000);
+// });
+// async function f() {
+//   const d = await p;
+//   console.log(d);
+// }
+// f();
+// console.log(7);
+
+//solution:
+// 7
+// 6
